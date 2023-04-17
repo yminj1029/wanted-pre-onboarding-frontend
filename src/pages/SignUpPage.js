c7ff103b19a011b515a92b22c28369336bbd91db
MinjiYun <minjiyun204@gmail.com>
2023-04-17 13:49:05 +0900

    회원가입 vaildataion 추가

--- src/pages/SignUpPage.js
+++ src/pages/SignUpPage.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import styled from 'styled-components';
 import { Link } from 'react-router-dom';
 
@@ -11,6 +11,7 @@ const SignUpDiv = styled.div`
         color: white;
         font-size: 25px;
         font-weight: bold;
+        margin-bottom:20px;
     }
 `;
 
@@ -32,6 +33,10 @@ const ButtonElement = styled.button`
     border: none;        
     background-color: #61dafb;
     color: white;
+    :disabled{
+        background-color: gray;
+        pointer-events:none;
+    }
     :hover{
         background-color: white;
         color: #61dafb;
@@ -43,12 +48,48 @@ const NavBarLink = styled(Link)`
 `;
 
 function SignUpPage(props) {
+    //email input check
+    const [emailAddress, setEmailAddress] = useState('');
+    const handleEmailInput = (e)=>{
+        setEmailAddress(e.target.value);
+    }
+    //email @포함
+    const isValidEmail = emailAddress.includes('@');
+
+    //pw input check
+    const [password, setPassword] = useState('');
+    const handlePassWord = (e)=>{
+        setPassword(e.target.value);
+    }
+    //비밀번호조건 8자 이상
+    const isValidPassword = password.length >= 8;
+
+    //회원가입 성공 후 sigin 페이지로 이동
+    const handleSignUp = ()=>{
+        console.log("email", emailAddress,"password", password)
+    }
+    
+
     return (
         <SignUpDiv>
             <span>Sign Up</span>
-            <InputElement data-testid="email-input" />
-            <InputElement data-testid="password-input" />
-            <ButtonElement data-testid="signup-button">회원가입</ButtonElement>
+            <InputElement 
+                onChange={handleEmailInput}
+                value={emailAddress}
+                placeholder='이메일 입력시 @를 포함해야 합니다.'
+                data-testid="email-input" />
+            <InputElement 
+                type='password'
+                onChange={handlePassWord}
+                value={password}
+                placeholder='비밀번호는 8자 이상이어야 합니다.'
+                data-testid="password-input" />
+            <ButtonElement
+                onClick={handleSignUp}
+                disabled={!isValidEmail||!isValidPassword} 
+                data-testid="signup-button">
+                회원가입
+            </ButtonElement>
             <NavBarLink to="/signin">로그인</NavBarLink>
         </SignUpDiv>
     );
